1619e89f232865245c43280620fa09360f2c675b
Tanzeb9277 <31424695+Tanzeb9277@users.noreply.github.com>
2023-01-25 13:15:07 -0500

    Update app.js

--- app.js
+++ app.js
@@ -30,7 +30,18 @@ const comment_controller = require('./controllers/commentController')
 const app = express();
 app.use(helmet());
 
-app.options('*', cors())
+var allowlist = ['https://tanzeb9277.github.io/', 'http://localhost:3000/']
+var corsOptionsDelegate = function (req, callback) {
+  var corsOptions;
+  if (allowlist.indexOf(req.header('Origin')) !== -1) {
+    corsOptions = { origin: true } // reflect (enable) the requested origin in the CORS response
+  } else {
+    corsOptions = { origin: false } // disable CORS for this request
+  }
+  callback(null, corsOptions) // callback expects two parameters: error and options
+}
+
+app.options('*', cors(corsOptionsDelegate))
 
 
 
